26122f261b4523cc95453be0f7a6f5a0f2384c4a
Elena Brazhnikova <lenovosibirsk@mail.ru>
2023-07-09 23:43:30 +0700

    add scroll behavor in to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss', '../../../styles.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  listener;
+  isScrolled: boolean = false;
+  getYPosition(e: Event): number {
+    return (e.target as Element).scrollTop;
+  }
+  constructor(private renderer2: Renderer2) {
+    this.listener = this.renderer2.listen('window', 'scroll', (e) => {
+      if (window.scrollY > 0) {
+        this.isScrolled = true;
+      } else if (window.scrollY === 0) {
+        this.isScrolled = false;
+      }
+    });
+  }
+}
